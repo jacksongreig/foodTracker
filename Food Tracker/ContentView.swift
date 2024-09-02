57918cecf7d8898495698a7075eda1aa3042cc45
Jackson Greig <69997932+jacksongreig@users.noreply.github.com>
2024-09-02 15:44:45 +1000

    Add files via upload

---
+++ Food Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Tracker
+//
+//  Created by Jackson Greig on 9/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food Tracker/Food_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_TrackerApp.swift
+//  Food Tracker
+//
+//  Created by Jackson Greig on 9/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
